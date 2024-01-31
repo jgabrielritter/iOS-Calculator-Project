43fd43c25efb6b207cf4076f18a92eb726b975d0
jgabrielritter <93882475+jgabrielritter@users.noreply.github.com>
2024-01-31 16:37:26 -0500

    Create .swift

---
+++ .swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorProject2App.swift
+//  CalculatorProject2
+//
+//  Created by Gabriel Ritter on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSCalculatorProject: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
